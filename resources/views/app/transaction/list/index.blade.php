bb44680bfc68e3e8ff1f8ff39986bccfb2b6fde8
napoleaofw <napoleao.wingert@gmail.com>
2017-11-11 00:04:31 -0200

    bootstrap test

---
+++ resources/views/app/transaction/list/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <!-- <link rel="stylesheet" type="text/css" href="{{ url('/app/...') }}"> -->
+    </head>
+    <body class="app">
+        <h1>welcome to the transaction list</h1>
+        <!-- <script type="text/javascript" src="{{ url('/app/...') }}"></script> -->
+    </body>
+</html>
